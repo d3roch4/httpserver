d200efea5831659cc89ba6ed87b3ba4f2c7fac16
André <d3roch4@gmail.com>
2019-08-20 18:30:07 -0300

    a

--- httpserver/session.h
+++ httpserver/session.h
@@ -27,7 +27,7 @@
 namespace httpserver {
 
 namespace beast = boost::beast;                 // from <boost/beast.hpp>
-namespace http = beast::http;                   // from <boost/beast/http.hpp>
+using namespace beast;                   // from <boost/beast/http.hpp>
 namespace websocket = beast::websocket;         // from <boost/beast/websocket.hpp>
 namespace net = boost::asio;                    // from <boost/asio.hpp>
 using tcp = boost::asio::ip::tcp;
