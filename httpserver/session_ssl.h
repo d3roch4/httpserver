5cf5488dad99328f15b2ecbcc5f1d56483811fa4
André <d3roch4@gmail.com>
2019-08-20 18:40:37 -0300

    a

--- httpserver/session_ssl.h
+++ httpserver/session_ssl.h
@@ -20,11 +20,7 @@
 
 namespace httpserver {
 
-namespace beast = boost::beast;         // from <boost/beast.hpp>
-namespace http = beast::http;           // from <boost/beast/http.hpp>
-namespace net = boost::asio;            // from <boost/asio.hpp>
 namespace ssl = boost::asio::ssl;       // from <boost/asio/ssl.hpp>
-using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 
 // Handles an HTTP server connection
 class session_ssl : public std::enable_shared_from_this<session_ssl>, public session
