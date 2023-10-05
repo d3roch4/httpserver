144b6dfcd1ad86ab51618f9f493f8761c4fb78bf
André Rocha <d3roch4@gmail.com>
2023-10-05 14:32:51 -0300

    Update deps.cmake
    change ssl deps

--- deps.cmake
+++ deps.cmake
@@ -7,22 +7,3 @@ FetchContent_GetProperties(d3util)
 FetchContent_Populate(d3util)
 add_subdirectory(${d3util_SOURCE_DIR} ${d3util_BINARY_DIR} EXCLUDE_FROM_ALL)
 
-
-
-find_package(OpenSSL QUIET)
-if(NOT OPENSSL_FOUND)
-    FetchContent_Declare (openssl
-            GIT_REPOSITORY "https://github.com/janbar/openssl-cmake.git"
-            )
-    FetchContent_GetProperties(openssl)
-    FetchContent_Populate(openssl)
-    add_subdirectory(${openssl_SOURCE_DIR} ${openssl_BINARY_DIR} EXCLUDE_FROM_ALL)
-    set(OPENSSL_SSL_LIBRARY ssl)
-    set(OPENSSL_CRYPTO_LIBRARY crypto)
-    set(OPENSSL_INCLUDE_DIR "${openssl_BINARY_DIR}/include" "${openssl_BINARY_DIR}")
-    set(OPENSSL_ROOT_DIR ${openssl_SOURCE_DIR})
-    set(OPENSSL_FOUND ON)
-    message(STATUS "Build OpenSSL: ${openssl_BINARY_DIR}")
-endif()
-set (HAVE_OPENSSL 1)
-include_directories (${OPENSSL_INCLUDE_DIR})
